ec4033d9660edc0c2a1538856d5bddd1b852ccc7
Stelios Sideras <stelios.sideras@gmail.com>
2016-08-03 21:25:28 +0000

    f

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 
-
+/*
+---
+Preview Application: https://calemd-ssp13-1.c9users.io/
+----
+we setup an angular app with tien.clndr and firebase injected as dependencies to external libraries these are referenced in index.html 
+*/
 app = angular.module('clndrApp', ['tien.clndr', 'firebase']);
 
+/*setup controller with dependencies as arguments */
+
 app.controller('DemoCtrl', function($scope, $firebaseObject, $firebaseArray) {
+    //options gia to clndr calendar directive
+     $scope.options = {
+        weekOffset: 1,
+        daysOfTheWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+        constraints: {
+            startDate: moment().subtract(1, 'months').format('YYYY-MM-16'),
+            endDate: moment().add(2, 'months').format('YYYY-MM-16')
+        }
+    };
+
     //reference to FIREFASE 
-    
     var ref = new Firebase("https://talkmedirty.firebaseio.com/days/");
     var ref1 = new Firebase("https://talkmedirty.firebaseio.com/calendar1/");
     
@@ -12,242 +28,50 @@ app.controller('DemoCtrl', function($scope, $firebaseObject, $firebaseArray) {
     we go one lvl deeper in object with child see object at console output 
     see firebase documentation regarding firebase arrays and objects
     */
-    $scope.data = $firebaseObject(ref.child('April'));
+    $scope.data = $firebaseObject(ref.child('July'));
     $scope.data2 = $firebaseArray(ref1.child('July'))
-    
-    
-    
-   
+    // var x = $scope.data2.$indexFor('-KKKYNlYm27R7TGd_jC6'); // returns location in the array
+    function findById (fireArray, id ) {
+        for(var i = 0; i < fireArray.length ; i++) { //ti trellla mou re  pano 
+              if(fireArray[i].$id === id){
+                  console.log("found",i)
+                  return i;
+              }
+          }
+    }
 
-    // $scope.data2.calendar = [{
-    // date: new Date('2016-07-01').getTime(),
-    // bookings: [{
-    //     time: "17:00",
-    //     booked: true,
-    //     bookingInfo: {
-    //         dateTime: null,
-    //         id: 1,
-    //         name: "",
-    //         surname:"",
-    //         email: "",
-    //         phone: ""
-    //     },
-    //     },
-    //         {
-    //             time: "21:00",
-    //             booked: false,
-    //             bookingInfo: {
-    //                 id: 2,
-    //                 name: "sdsds",
-    //                 surname:"korompos",
-    //                 email: "stad.@grfgr.gr",
-    //                 phone: "453543543",
-    //                 dateBooked: Firebase.ServerValue.TIMESTAMP
-    //             }
-    //         },
-
-    //         {
-    //             time: "01:00",
-    //             booked: false,
-    //             bookingInfo: {
-    //                 dateTime:  new Date(2016, 04, 01, 17, 00).getTime(),
-    //                 id: 3,
-    //                 name: "sdsds",
-    //                 surname:"korompos",
-    //                 email: "stad.@grfgr.gr",
-    //                 phone: "453543543"
-    //             }
-    //         }]
-    //     },
-    //     {
-    //         date:  new Date('2016-05-03').getTime(),
-    //         bookings: [{
-    //             dateTime:  new Date(2016, 04, 03, 17, 00).getTime(),
-    //             time: "17:00",
-    //             booked: true,
-    //             bookingInfo: {
-    //                 id: 4,
-    //                 name: "sdsds",
-    //                 surname:"korompos",
-    //                 email: "stad.@grfgr.gr",
-    //                 phone: "453543543"
-    //             },
-    //         },
-    //             {
-    //             dateTime:  new Date(2016, 04, 03, 17, 00).getTime(),
-    //             time: "21:00",
-    //             booked: true,
-    //             bookingInfo: {
-    //                 id: 5,
-    //                 name: "sdsds",
-    //                 surname:"korompos",
-    //                 email: "stad.@grfgr.gr",
-    //                 phone: "453543543"
-    //             }
-    //         },
-
-    //             {
-    //             time: "01:00",
-    //             booked: true,
-    //             bookingInfo: {
-    //                 id: 6,
-    //                 name: "sdsds",
-    //                 surname:"korompos",
-    //                 email: "stad.@grfgr.gr",
-    //                 phone: "453543543"
-    //             }
-    //         }]
-    //     },
-    //     {
-    //         date:  new Date('2016-05-02').getTime(),
-    //         bookings: [{
-    //             dateTime:  new Date(2016, 04, 02, 17, 00).getTime(),
-    //             time: "17:00",
-    //             booked: true,
-    //             bookingInfo: {
-    //                 id: 7,
-    //                 name: "sdsds",
-    //                 surname:"korompos",
-    //                 email: "stad.@grfgr.gr",
-    //                 phone: "453543543"
-    //             },
-    //         },
-    //             {
-    //                 dateTime:  new Date(2016, 04, 02, 21, 00).getTime(),
-    //                 time: "21:00",
-    //                 booked: false,
-    //                 bookingInfo: {
-    //                     id: 8,
-    //                     name: "sdsds",
-    //                     surname:"korompos",
-    //                     email: "stad.@grfgr.gr",
-    //                     phone: "453543543"
-    //                 }
-    //             },
-    //             {
-    //                 dateTime:  new Date(2016, 05, 02, 21, 00).getTime(),
-    //                 time: "21:00",
-    //                 booked: false,
-    //                 bookingInfo: {
-    //                     id: 8,
-    //                     name: "sdsds",
-    //                     surname:"korompos",
-    //                     email: "stad.@grfgr.gr",
-    //                     phone: "453543543"
-    //                 }
-    //             },
-    //             {
-    //                 dateTime:  new Date(2016, 04, 02, 01, 00).getTime(),
-    //                 time: "01:00",
-    //                 booked: true,
-    //                 bookingInfo: {
-    //                     id: 9,
-    //                     name: "sdsds",
-    //                     surname:"korompos",
-    //                     email: "stad.@grfgr.gr",
-    //                     phone: "453543543"
-    //                 }
-    //             },
-    //              {
-    //                 dateTime:  new Date(2016, 05, 02, 01, 00).getTime(),
-    //                 time: "01:00",
-    //                 booked: true,
-    //                 bookingInfo: {
-    //                     id: 9,
-    //                     name: "sdsds",
-    //                     surname:"korompos",
-    //                     email: "stad.@grfgr.gr",
-    //                     phone: "453543543"
-    //                 }
-    //             }
-    //             ]
-    //     }
-
-    // ]
-
-
-    // $scope.data2.$add({
-    //     date: new Date('2016-07-01').getTime(),
-    //     bookings: [{
-    //         time: "17:00",
-    //         booked: false,
-    //         bookingInfo: {
-    //             dateTime: null,
-    //             id: 1,
-    //             name: "",
-    //             surname: "",
-    //             email: "",
-    //             phone: ""
-    //         }
-    //     },
-    //     ]
-
-    // });
-    // var i =0
-    // var now = new Date();
-    // var daysOfYear = [];
-    // for (var d = new Date('2016-07-01'); d <= new Date('2016-08-01'); d.setDate(d.getDate() + 1)) 
-    // {
-    //     daysOfYear.push(new Date(d));
-    //       $scope.data2.$add({
-    //         date: new Date(d).getTime(),
-    //         bookings: [{
-    //             time: "17:00",
-    //             booked: false,
-    //             bookingInfo: {
-    //                 dateTime: null,
-    //                 id: 1,
-    //                 name: "",
-    //                 surname: "",
-    //                 email: "",
-    //                 phone: ""
-    //             }
-    //         },
-    //         ]
-    
-    //     });
-        
-    // }
-
-  //Temp array to hold events 
+  /*Temp array to hold events */
   var events = [];
-  
   /**we resolve the data*/
-    $scope.data2.$loaded()
-      .then(function(x) {
-       console.log("READY")
+    $scope.data2.$loaded().then(function(x) {
+        console.log("54",x)
+        $scope.finished = true;
        /*
        we map data from firebase array to our events array to be fed in clndr calendar library
        _.each see underscore js used for iteration
        **/
       _.each($scope.data2, function(elem) {
             _.each(elem.bookings, function(el) {
-                var vc = {
-                    date: new Date(elem.date),
-                    time: el.time,
-                    available: !el.booked,
-                    dateTime: el.dateTime,
-                    bookingInfo: el.bookingInfo
+                var mapObject = {
+                        date: new Date(elem.date),
+                        time: el.time,
+                        available: !el.available,
+                        dateTime: el.date,
+                        bookingInfo: el.bookingInfo,
+                        id : elem.$id
+                    
                 };
-            
-                events.push(vc);
+                events.push(mapObject);
                 $scope.events = events;
-            })
-           
+            });
         });
       })
       .catch(function(error) {
         console.log("Error:", error);
       });
 
-    $scope.data2.$save();
-    
-   
-    
-     
-    /**this function in run from the html template i.e index.html */
+   /**this function in run from the html template i.e index.html */
     $scope.availableEvents = function(day) {
-        //console.log("day",day)
         var count = day.events.length;
         _.each(day.events, function(elem) {
             if (elem.available == false) {
@@ -262,20 +86,18 @@ app.controller('DemoCtrl', function($scope, $firebaseObject, $firebaseArray) {
         //        }
         //    })
     }
-    
-  
-    
-    console.log("Events",$scope.events)
-    
+      $scope.event = {};
     /*function use on ng-click in template ie. index.html*/
     $scope.select = function(item) {
-        $scope.event.showDetails = false;
-        $scope.event.bookingDetails = false;
-        $scope.selected = item;
+        $scope.event.showDetails = false
+        $scope.event.bookingDetails = false
+        $scope.selected = item
+        
     }
     
     $scope.isActive = function(item) {
         return $scope.selected === item;
+        
     }
 
     /*MOCK DATA */
@@ -288,37 +110,62 @@ app.controller('DemoCtrl', function($scope, $firebaseObject, $firebaseArray) {
     
     //  }
     // };
-    
 
-    
    
     
-    $scope.options = {
-        weekOffset: 1,
-        daysOfTheWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-        constraints: {
-            startDate: moment().subtract(1, 'months').format('YYYY-MM-16'),
-            endDate: moment().add(2, 'months').format('YYYY-MM-16')
-        }
+     
+    $scope.submit = function(event) {
+        //kalimera des edw 
+        /*///PANOS 
+            TO DO USE 
+            findById () to find the index σε ποια θεση βρισκεται array το event poy paei na kanei book o typos  to event exei ena $id 
+        */
+        var index = findById( $scope.data2,event.id);
+        event.available = false
+        $scope.bookingInfo = angular.copy(event);
+        console.log("event",event)
+        
+        var x = $scope.event;
+        var bookingInfo = {
+            booked:true,
+            
+            name: $scope.event.bookingInfo.name,
+            surname:  $scope.event.bookingInfo.surname,
+            email:  $scope.event.bookingInfo.email,
+            phone:  $scope.event.bookingInfo.phone
+        };
+        
+        $scope.data2[index].bookings[0].bookingInfo = bookingInfo;
+         $scope.data2[index].bookings[0].bookingInfo.booked = true;
+        // _.each($scope.data2.calendar, function(elem) {
+        //         console.log($scope.event.bookingInfo.id)
+        //         _.each(elem.bookings, function(el) {
+        //             console.log($scope.event.bookingInfo.id)
+        //             if (el.bookingInfo.id == $scope.event.bookingInfo.id) {
+        //                 el.bookingInfo = bookingInfo;
+        //                 console.log(el.bookingInfo.id, ":FOUD")
+        //             }
+        //         })
+        //     })
+            //console.log("find",_.findWhere($scope.data2.calendar,{id:$scope.event.bookingInfo.id}))
+    
+        // $scope.data2[0].bookings[0].bookingInfo = bookingInfo;
+        
+        /// EDW KANOUME SAVE STI BASI   to     .then einai kane save kai otan telioseis trexe ayti ti function() see promises andular
+        $scope.data2.$save(index).then(function(ref) {
+            console.log(ref.key());
+        })
     }
-
-    $scope.showDetails = false;
-    $scope.submit = function() {
-       
-    };
-    $scope.event = {};
+    
     $scope.isSelectedEvent = function(event) {
         if (event.available == false) {
             return;
         }
         return $scope.event === event ? 'active' : ""
     }
-    $scope.dayAvailable = function(day) {
-       
-        //$scope.availableDay = false;
-        //console.log(day)
+    
+    $scope.dayAvailable = function( day ) {
         _.each(day.events, function(elem) {
-            //console.log(elem)
             if (elem.available === true) {
                 $scope.availableDay = true;
             }
@@ -327,8 +174,10 @@ app.controller('DemoCtrl', function($scope, $firebaseObject, $firebaseArray) {
     }
 
     $scope.eventSelected = false;
-    $scope.selectEvent = function(event) {
-        console.log($scope.showBookingDetails)
+    $scope.showDetails = false;
+    
+    $scope.selectEvent = function( event ) {
+
         if (event.available == false) {
             event.bookingVisible = !event.bookingVisible;
             $scope.event = event;
@@ -339,17 +188,16 @@ app.controller('DemoCtrl', function($scope, $firebaseObject, $firebaseArray) {
             event.bookingVisible = false;
             $scope.showDetails = true;
             event.showDetails = !event.showDetails;
-            //$scope.eventSelected = event;
-            // event.available= false;
             $scope.event = event;
         }
-
     }
     
     $scope.showEvents = function(events) {
+  
         $scope.localData = {
             showEvents: true
         }
+  
         $scope.ti = events;
     }
 })
